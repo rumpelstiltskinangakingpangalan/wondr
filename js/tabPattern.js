d40bfae4b5c0e456e0d7a76582370dabc32bbfdd
rumpelstiltskinangakingpangalan <104667993+rumpelstiltskinangakingpangalan@users.noreply.github.com>
2024-03-06 22:49:55 +0800

    Add files via upload

--- js/tabPattern.js
+++ js/tabPattern.js
@@ -16,33 +16,4 @@ for(let a = 0; a < arrPatterns.length; a++) {
 
 
 
-//============DROPDOWN SELECTION==============
-var dropPattern = document.getElementById('dropPattern');
-
-function hideOptionPattern(e) {
-
-    if(e.target.id !== dropPattern.children[2].id || e.target.id === dropPattern) {
-        dropPattern.children[2].style.display = "none";
-    }
-
-}
-
-function showOptionPattern() {
-    
-    if(dropPattern.children[2].style.display == "none") {
-        
-        dropPattern.children[2].style.display = "flex";
-        dropPattern.children[2].style.left = `${dropPattern.offsetLeft}px`;
-        dropPattern.children[2].style.top = `${dropPattern.offsetHeight + dropPattern.offsetTop + 1}px`;
-        
-    }
-    else {
-        dropPattern.children[2].style.display = "none";
-    }
-
-}
-
-dropPattern.children[2].style.display = "none";
-dropPattern.addEventListener('click', showOptionPattern);
-
 
